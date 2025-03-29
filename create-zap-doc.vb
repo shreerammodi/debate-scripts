15fa2078027ef7e03ca567e88210fc1293b41f1a
Shreeram Modi <smodi@smodi.net>
2025-03-29 15:59:54 -0400

    zap-doc: set default highlight color
    Without a default highlight color, the macro fails to highlight the
    styles that are to be maintained in the zapped document.

--- create-zap-doc.vb
+++ create-zap-doc.vb
@@ -6,6 +6,7 @@ Sub Zap()
 
     ' Styles that should be preserved in the read doc
     styles = Array("Pocket", "Hat", "Block", "Tag", "Cite", "Analytic", "Undertag")
+    Options.DefaultHighlightColorIndex = wdTurquoise
 
     ' First pass: turn on highlights for styles
     For Each s In styles
